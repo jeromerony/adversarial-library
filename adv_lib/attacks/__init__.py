98628fcb14de84e02223f97b628944f7afb37f64
Jerome Rony <jerome.rony@gmail.com>
2020-12-08 14:52:24 -0500

    Add attacks to init

--- adv_lib/attacks/__init__.py
+++ adv_lib/attacks/__init__.py
@@ -1,4 +1,7 @@
+from .augmented_lagrangian_attacks import alma
 from .auto_pgd import apgd
 from .carlini_wagner import carlini_wagner_l2, carlini_wagner_linf
 from .decoupled_direction_norm import ddn
+from .fast_adaptive_boundary import fab
+from .perceptual_color_attacks import perc_al
 from .projected_gradient_descent import pgd_linf
