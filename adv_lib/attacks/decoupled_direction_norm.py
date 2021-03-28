c2fa23a56e878d694ea2014a8a8bd289626bc9ca
Jerome Rony <jerome.rony.1@etsmtl.net>
2021-03-28 17:35:53 -0400

    Rework implementation for clarity w.r.t the paper

--- adv_lib/attacks/decoupled_direction_norm.py
+++ adv_lib/attacks/decoupled_direction_norm.py
@@ -1,7 +1,8 @@
+import math
 from typing import Optional
 
 import torch
-from torch import nn, optim, Tensor
+from torch import nn, Tensor
 from torch.autograd import grad
 from torch.nn import functional as F
 
@@ -52,14 +53,10 @@ def ddn(model: nn.Module,
     batch_view = lambda tensor: tensor.view(batch_size, *[1] * (inputs.ndim - 1))
 
     # Init variables
-    multiplier = 1 if targeted else -1
+    multiplier = -1 if targeted else 1
     δ = torch.zeros_like(inputs, requires_grad=True)
     ε = torch.full((batch_size,), init_norm, device=device, dtype=torch.float)
     worst_norm = torch.max(inputs, 1 - inputs).flatten(1).norm(p=2, dim=1)
-
-    # Setup optimizer
-    optimizer = optim.SGD([δ], lr=1)
-    scheduler = optim.lr_scheduler.CosineAnnealingLR(optimizer, T_max=steps, eta_min=0.01)
 
     # Init trackers
     best_l2 = worst_norm.clone()
@@ -67,6 +64,7 @@ def ddn(model: nn.Module,
     adv_found = torch.zeros(batch_size, dtype=torch.bool, device=device)
 
     for i in range(steps):
+        α = torch.tensor(0.01 + (1 - 0.01) * (1 + math.cos(math.pi * i / steps)) / 2, device=device)
 
         l2 = δ.data.flatten(1).norm(p=2, dim=1)
         adv_inputs = inputs + δ
@@ -82,31 +80,28 @@ def ddn(model: nn.Module,
         best_l2 = torch.where(is_both, l2, best_l2)
         best_δ = torch.where(batch_view(is_both), δ.detach(), best_δ)
 
-        optimizer.zero_grad()
-        δ.grad = grad(loss.sum(), δ, only_inputs=True)[0]
+        δ_grad = grad(loss.sum(), δ, only_inputs=True)[0]
         # renorming gradient
-        grad_norms = δ.grad.flatten(1).norm(p=2, dim=1)
-        δ.grad.div_(batch_view(grad_norms))
+        grad_norms = δ_grad.flatten(1).norm(p=2, dim=1)
+        δ_grad.div_(batch_view(grad_norms))
         # avoid nan or inf if gradient is 0
         if (zero_grad := (grad_norms < 1e-12)).any():
-            δ.grad[zero_grad] = torch.randn_like(δ.grad[zero_grad])
+            δ_grad[zero_grad] = torch.randn_like(δ_grad[zero_grad])
 
         if callback is not None:
-            cosine = F.cosine_similarity(-δ.grad.flatten(1), δ.data.flatten(1), dim=1).mean()
+            cosine = F.cosine_similarity(δ_grad.flatten(1), δ.data.flatten(1), dim=1).mean()
             callback.accumulate_line('ce', i, ce_loss.mean())
             callback_best = best_l2.masked_select(adv_found).mean()
-            callback.accumulate_line(['epsilon', 'l2', 'best_l2'], i, [ε.mean(), l2.mean(), callback_best])
-            lr = torch.tensor(scheduler.get_last_lr()[0], device=device)
-            callback.accumulate_line(['cosine', 'lr', 'success'], i, [cosine, lr, adv_found.float().mean()])
+            callback.accumulate_line(['ε', 'l2', 'best_l2'], i, [ε.mean(), l2.mean(), callback_best])
+            callback.accumulate_line(['cosine', 'α', 'success'], i, [cosine, α, adv_found.float().mean()])
 
             if (i + 1) % (steps // 20) == 0 or (i + 1) == steps:
                 callback.update_lines()
 
-        optimizer.step()
-        scheduler.step()
+        δ.data.add_(δ_grad, alpha=α)
 
-        ε.mul_(1 - (2 * is_adv - 1) * γ)
-        ε = torch.min(ε, worst_norm)
+        ε = torch.where(is_adv, (1 - γ) * ε, (1 + γ) * ε)
+        ε = torch.minimum(ε, worst_norm)
 
         δ.data.mul_(batch_view(ε / δ.data.flatten(1).norm(p=2, dim=1)))
         δ.data.add_(inputs).clamp_(0, 1)
