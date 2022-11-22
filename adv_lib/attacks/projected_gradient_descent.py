c4abfadf79d1df48f7e98abc99d29173cfcad460
Jerome Rony <jerome.rony.1@etsmtl.net>
2022-11-22 15:20:46 -0500

    Make PGD compatible with Tensor ε

--- adv_lib/attacks/projected_gradient_descent.py
+++ adv_lib/attacks/projected_gradient_descent.py
@@ -1,19 +1,20 @@
 from functools import partial
-from typing import Optional, Tuple
+from typing import Optional, Tuple, Union
 
 import torch
-from torch import nn, Tensor
+from torch import Tensor, nn
 from torch.autograd import grad
 from torch.nn import functional as F
 
 from adv_lib.utils.losses import difference_of_logits, difference_of_logits_ratio
+from adv_lib.utils.projections import clamp_
 from adv_lib.utils.visdom_logger import VisdomLogger
 
 
 def pgd_linf(model: nn.Module,
              inputs: Tensor,
              labels: Tensor,
-             ε: float,
+             ε: Union[float, Tensor],
              targeted: bool = False,
              steps: int = 40,
              random_init: bool = True,
@@ -27,13 +28,18 @@ def pgd_linf(model: nn.Module,
 
     adv_inputs = inputs.clone()
     adv_found = torch.zeros(batch_size, dtype=torch.bool, device=device)
-    pgd_attack = partial(_pgd_linf, model=model, ε=ε, targeted=targeted, steps=steps, random_init=random_init,
+
+    if isinstance(ε, (int, float)):
+        ε = torch.full_like(adv_found, ε, dtype=inputs.dtype)
+
+    pgd_attack = partial(_pgd_linf, model=model, targeted=targeted, steps=steps, random_init=random_init,
                          loss_function=loss_function, relative_step_size=relative_step_size,
                          absolute_step_size=absolute_step_size)
 
     for i in range(restarts):
 
-        adv_found_run, adv_inputs_run = pgd_attack(inputs=inputs[~adv_found], labels=labels[~adv_found])
+        adv_found_run, adv_inputs_run = pgd_attack(inputs=inputs[~adv_found], labels=labels[~adv_found],
+                                                   ε=ε[~adv_found])
         adv_inputs[~adv_found] = adv_inputs_run
         adv_found[~adv_found] = adv_found_run
 
@@ -49,7 +55,7 @@ def pgd_linf(model: nn.Module,
 def _pgd_linf(model: nn.Module,
               inputs: Tensor,
               labels: Tensor,
-              ε: float,
+              ε: Tensor,
               targeted: bool = False,
               steps: int = 40,
               random_init: bool = True,
@@ -65,7 +71,7 @@ def _pgd_linf(model: nn.Module,
     device = inputs.device
     batch_size = len(inputs)
     batch_view = lambda tensor: tensor.view(batch_size, *[1] * (inputs.ndim - 1))
-    clamp = lambda tensor: tensor.data.clamp_(min=-ε, max=ε).add_(inputs).clamp_(min=0, max=1).sub_(inputs)
+    lower, upper = torch.maximum(-inputs, -batch_view(ε)), torch.minimum(1 - inputs, batch_view(ε))
 
     loss_func, multiplier = _loss_functions[loss_function.lower()]
 
@@ -78,30 +84,29 @@ def _pgd_linf(model: nn.Module,
         step_size *= -1
 
     δ = torch.zeros_like(inputs, requires_grad=True)
-    δ_adv = torch.zeros_like(inputs)
+    best_adv = inputs.clone()
     adv_found = torch.zeros(batch_size, dtype=torch.bool, device=device)
 
     if random_init:
-        δ.data.uniform_(-ε, ε)
-        clamp(δ)
-    else:
-        δ.data.zero_()
+        δ.data.uniform_(-1, 1).mul_(batch_view(ε))
+        clamp_(δ, lower=lower, upper=upper)
 
     for i in range(steps):
-        logits = model(inputs + δ)
+        adv_inputs = inputs + δ
+        logits = model(adv_inputs)
 
         if i == 0 and loss_function.lower() in ['dl', 'dlr']:
             labels_infhot = torch.zeros_like(logits).scatter_(1, labels.unsqueeze(1), float('inf'))
             loss_func = partial(loss_func, labels_infhot=labels_infhot)
 
         loss = multiplier * loss_func(logits, labels)
-        δ_grad = grad(loss.sum(), δ, only_inputs=True)[0]
+        δ_grad = grad(loss.sum(), δ, only_inputs=True)[0].sign_().mul_(batch_view(step_size))
 
         is_adv = (logits.argmax(1) == labels) if targeted else (logits.argmax(1) != labels)
-        δ_adv = torch.where(batch_view(is_adv), δ.detach(), δ_adv)
+        best_adv = torch.where(batch_view(is_adv), adv_inputs.detach(), best_adv)
         adv_found.logical_or_(is_adv)
 
-        δ.data.add_(δ_grad.sign(), alpha=step_size)
-        clamp(δ)
+        δ.data.add_(δ_grad)
+        clamp_(δ, lower=lower, upper=upper)
 
-    return adv_found, inputs + δ_adv
+    return adv_found, best_adv
