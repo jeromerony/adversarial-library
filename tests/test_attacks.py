e73c2956d39f2e6ba6fa477c1ff1aa412b5712bb
Jérôme Rony <jerome@rony.fr>
2026-06-07 14:22:05 -0400

    Adjust test and budgets

--- tests/test_attacks.py
+++ tests/test_attacks.py
@@ -1,3 +1,4 @@
+import logging
 import math
 from functools import partial
 from typing import Callable
@@ -8,6 +9,8 @@ from torch import nn
 from torch.nn import functional as F
 
 from adv_lib import attacks
+
+logger = logging.getLogger(__name__)
 
 
 class Linear(nn.Module):
@@ -30,11 +33,11 @@ class Linear(nn.Module):
 
 
 _attacks_untargeted_minimal_l2 = (
-    partial(attacks.ddn, steps=30),
+    partial(attacks.ddn, steps=50),
     partial(attacks.alma, num_steps=50),
     partial(attacks.df, steps=10),
-    partial(attacks.carlini_wagner_l2, max_iterations=30),
-    partial(attacks.tr, iter=10, eps=0.1),
+    partial(attacks.carlini_wagner_l2, max_iterations=20, learning_rate=0.1),
+    partial(attacks.tr, iter=50, eps=0.1),
     partial(attacks.fab, norm=2, n_iter=10),
     partial(attacks.fmn, norm=2, steps=10),
     partial(attacks.pdgd, num_steps=200),
@@ -49,7 +52,7 @@ _attacks_untargeted_minimal_l2 = (
 def test_untargeted_minimal_l2(attack: Callable, batch_size: int, dims: tuple[int]):
     torch.manual_seed(0)
     model = Linear(input_dim=math.prod(dims))
-    inputs = torch.randn(batch_size, *dims).mul_(0.01).add_(0.55).clamp_(min=0, max=1)
+    inputs = torch.randn(batch_size, *dims).mul_(0.03).add_(0.75).clamp_(min=0, max=1)
     labels = inputs.new_ones(batch_size, dtype=torch.long)
 
     preds = model(inputs).argmax(dim=1)
@@ -63,6 +66,8 @@ def test_untargeted_minimal_l2(attack: Callable, batch_size: int, dims: tuple[in
     pred_projection = model(projection)[..., :2]
     torch.testing.assert_close(pred_projection, torch.zeros_like(pred_projection))
 
-    best_norm = (projection - inputs).norm(p=2, dim=1)
-    adv_norm = (adv_inputs - inputs).norm(p=2, dim=1)
-    assert (adv_norm <= best_norm * 2).all()
+    best_norm = (projection - inputs).flatten(1).norm(p=2, dim=1)
+    adv_norm = (adv_inputs - inputs).flatten(1).norm(p=2, dim=1)
+    ratio = adv_norm / best_norm
+    logger.info(f"Ratio of adv norm over best: {ratio.numpy(force=True)}")
+    assert (ratio <= 1.25).all()
